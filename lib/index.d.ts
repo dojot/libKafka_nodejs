5aff2c98dba3e52316c0f8b57716f244f3268581
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-08-23 13:11:40 -0300

    adding return value to index.d.ts createTopic function and updating version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 declare module "@dojot/adminkafka" {
   class Admin {
     constructor(kafkaAddress: string, kakfaPort: number);
-    createTopic(topic: string, partitions: number, replicaFactor: number);
+    createTopic(topic: string, partitions: number, replicaFactor: number): number;
   }
 }
